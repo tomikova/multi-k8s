42cf3f95eddcd6994c5ea98d2c776f489b0c8e3b
Tomislav <tomislavk7@gmail.com>
2023-01-27 01:18:13 +0100

    homepage change

--- client/src/App.js
+++ client/src/App.js
@@ -10,7 +10,7 @@ function App() {
       <div className="App">
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
-          <Link to="/">Fib calculator version 2</Link>
+          <Link to="/">Fib calculator version Kubernetes</Link>
           <Link to="/otherpage">Other Page</Link>
         </header>
         <div>
